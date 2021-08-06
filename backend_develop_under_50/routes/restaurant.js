a9d5ff508c1332a86a0c438c2d964517a4c81843
Shreyansh-Mishra <201951146@iiitvadodara.ac.in>
2021-08-07 00:28:06 +0530

    add more routes

--- backend_develop_under_50/routes/restaurant.js
+++ backend_develop_under_50/routes/restaurant.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var { addItem, getAllRestaurants} = require('../controllers/restaurant/restaurant.js');
+var { addItem, getAllRestaurants, getRestaurant, getRestaurantByName} = require('../controllers/restaurant/restaurant.js');
 /* GET users listing. */
 
 router.post('/add/new', );
@@ -8,5 +8,9 @@ router.post('/add/new', );
 router.post('/add/item', addItem);
 
 router.get('/get/all', getAllRestaurants);
+
+router.get('/get/id/:id', getRestaurant);
+
+router.get('/get/name/:name', getRestaurantByName);
 
 module.exports = router;
